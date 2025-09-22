290a467deac4b1d08f990e4e91af401e3550743a
Sree Raj Muthaiya.A.L <sreerajmuthaiya.a.l@Srees-MacBook-Pro.local>
2025-09-22 23:56:34 +0530

    add analytics

--- CH/src/main.tsx
+++ CH/src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { inject } from "@vercel/analytics";
+
+inject();
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
